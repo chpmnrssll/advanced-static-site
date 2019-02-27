d4ba6c72b5aa99825c535740a807d3f7da7a7cd7
Russell Chapman <chpmnrssll@gmail.com>
2019-02-27 02:37:22 -0700

    Added rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,15 @@ const config = {
   "extends": "airbnb-base",
   "env": {
     "browser": true,
-    "node": true
+    "node": true,
   },
   "globals": {
-    "document": false
-  }
+    "document": false,
+  },
+  "rules": {
+    "no-param-reassign": 0,
+    "no-plusplus": 0,
+  },
 };
 
 module.exports = config;
