96b249e0cf33424e5e7ce3ddc0d63b6d81345a4a
Russell Chapman <chpmnrssll@gmail.com>
2019-02-26 23:27:48 -0700

    Added popmotion, testing

--- _src/index.js
+++ _src/index.js
@@ -1,2 +1,14 @@
 // CSS and SASS files
 import './index.scss';
+import { easing, styler, tween } from 'popmotion';
+
+const element = document.querySelector('.home__cta');
+const elementStyler = styler(element);
+
+tween({
+  from: { rotate: -5 },
+  to: { rotate: 5 },
+  ease: easing.easeInOut,
+  flip: Infinity,
+  duration: 1500,
+}).start(v => elementStyler.set(v));
