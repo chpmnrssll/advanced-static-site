f4950fc666204ca24a22a282fa62196e7b0a69be
Russell Chapman <chpmnrssll@gmail.com>
2019-03-02 00:28:53 -0700

    Changed headline animation

--- _src/animateHeadline.js
+++ _src/animateHeadline.js
@@ -1,5 +1,30 @@
-import { easing, styler, tween } from 'popmotion';
 import splitter from 'text-split';
+import { keyframes, styler } from 'popmotion';
+
+function shuffleLetters(thisStyler, stagger) {
+  const r = Math.random();
+  const angle = (r - 0.5) * 15;
+  const animation = keyframes({
+    values: [
+      { scale: 1 },
+      { scale: 1.25 },
+      { scale: 1, rotate: angle },
+      { scale: 1.1, rotate: angle },
+      { scale: 1, rotate: angle },
+    ],
+    duration: 384,
+    times: [0, 0.7, 0.8, 0.9, 1],
+  });
+
+  setTimeout(() => {
+    animation.start({
+      update: v => thisStyler.set(v),
+      complete: () => {
+        thisStyler.set({ rotate: angle });
+      },
+    });
+  }, stagger);
+}
 
 export default function () {
   const headline = document.querySelector('.home__headline');
@@ -8,17 +33,12 @@ export default function () {
     const letters = splitter(headline);
     const elementStylers = Array.from(letters).map(styler);
 
-    elementStylers.forEach((thisStyler) => {
-      const r = Math.random();
-      const angle = 3 + (r - 0.5) * 12;
+    elementStylers.forEach((thisStyler, i) => {
+      shuffleLetters(thisStyler, i * 16);
 
-      tween({
-        from: { rotate: -angle },
-        to: { rotate: angle },
-        ease: easing.easeInOut,
-        flip: Infinity,
-        duration: 1000 + r * 500,
-      }).start(v => thisStyler.set(v));
+      setInterval(() => {
+        shuffleLetters(thisStyler, i * 16);
+      }, 15000);
     });
   }
 }
