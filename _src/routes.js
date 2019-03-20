be961484f11ffd7b4ec7a1dd757e8cadb8f2cbe0
Russell Chapman <chpmnrssll@gmail.com>
2019-03-19 23:10:41 -0600

    Added medium-zoom

--- _src/routes.js
+++ _src/routes.js
@@ -1,12 +1,19 @@
 import page from 'page';
+import mediumZoom from 'medium-zoom';
 import animateHeadlines from './animateHeadlines';
 import animateFooter from './animateFooter';
 import { animateContentIn, animateContentOut } from './animateContent';
 import { fitTextElements, fitTextFatElements, stopFitting } from './featured';
 import runDemos from './canvasDemos';
 
+let zoom = null;
+
 // Lazy load and animate in new content
 page('*', (context) => {
+  zoom = mediumZoom('.image__thumb, .image__thumb--alt', {
+    background: '#000000aa',
+  });
+
   // Initial page load
   if (context.init) {
     fitTextElements();
@@ -42,6 +49,7 @@ page('*', (context) => {
 page.exit('*', (context, next) => {
   animateContentOut().then(() => {
     stopFitting();
+    zoom.detach();
     if (window.demo && window.demo.stop) {
       window.demo.stop();
     }
