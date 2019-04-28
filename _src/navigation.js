ca4cd751422baddfe55a15428c51fd0bfb76a50f
Russell Chapman <chpmnrssll@gmail.com>
2019-04-27 19:28:25 -0600

    Replaced vanilla js scrollTo with popmotion version

--- _src/navigation.js
+++ _src/navigation.js
@@ -1,3 +1,5 @@
+import { easing, styler, tween } from 'popmotion';
+
 export default class Navigation {
   constructor() {
     // this.showButton = document.querySelector('.navigation__button--open');
@@ -18,15 +20,24 @@ export default class Navigation {
     this.currentX = 0;
     this.startX = 0;
 
-    // this.showButton.addEventListener('click', this.show);
-    // this.hideButton.addEventListener('click', this.hide);
+
     this.hamburgerButton.addEventListener('click', this.toggle);
     this.navigation.addEventListener('click', this.hide);
     this.navigation.addEventListener('touchstart', this.onTouchStart, { passive: true });
     this.navigation.addEventListener('touchmove', this.onTouchMove, { passive: true });
     this.navigation.addEventListener('touchend', this.onTouchEnd);
     this.topButton.addEventListener('click', () => {
-      this.scrollTo(0, 500, 'easeInOutQuad');
+      const viewportStyler = styler(window);
+      const viewportAnimation = tween({
+        from: window.scrollY,
+        to: 0,
+        ease: easing.easeInOut,
+        duration: 512,
+      });
+
+      viewportAnimation.start({
+        update: v => viewportStyler.set('scrollTop', v),
+      });
     }, { passive: true });
   }
 
@@ -73,79 +84,5 @@ export default class Navigation {
     this.navigation.classList.add('navigation--hidden');
     this.hamburgerButton.classList.remove('is-active');
     this.showing = false;
-  }
-
-  // https://pawelgrzybek.com/page-scroll-in-vanilla-javascript/
-  static scrollTo(destination, duration = 200, easing = 'linear') {
-    const easings = {
-      linear(t) {
-        return t;
-      },
-      easeInQuad(t) {
-        return t * t;
-      },
-      easeInCubic(t) {
-        return t * t * t;
-      },
-      easeInQuart(t) {
-        return t * t * t * t;
-      },
-      easeInQuint(t) {
-        return t * t * t * t * t;
-      },
-      easeOutQuad(t) {
-        return t * (2 - t);
-      },
-      easeOutCubic(t) {
-        return (--t) * t * t + 1;
-      },
-      easeOutQuart(t) {
-        return 1 - (--t) * t * t * t;
-      },
-      easeOutQuint(t) {
-        return 1 + (--t) * t * t * t * t;
-      },
-      easeInOutQuad(t) {
-        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-      },
-      easeInOutCubic(t) {
-        return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-      },
-      easeInOutQuart(t) {
-        return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
-      },
-      easeInOutQuint(t) {
-        return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-      },
-    };
-
-    const start = window.pageYOffset;
-    const startTime = 'now' in window.performance ? window.performance.now() : new Date().getTime();
-    const documentHeight = Math.max(document.body.scrollHeight,
-      document.body.offsetHeight,
-      document.documentElement.clientHeight,
-      document.documentElement.scrollHeight,
-      document.documentElement.offsetHeight);
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-    const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-
-    let scrollOffset = destinationOffset;
-    if (documentHeight - destinationOffset < windowHeight) {
-      scrollOffset = documentHeight - windowHeight;
-    }
-    const destinationOffsetToScroll = Math.round(scrollOffset);
-
-    function tick() {
-      const now = 'now' in window.performance ? window.performance.now() : new Date().getTime();
-      const time = Math.min(1, ((now - startTime) / duration));
-      const timeFunction = easings[easing](time);
-      window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
-
-      if (window.pageYOffset !== destinationOffsetToScroll) {
-        window.requestAnimationFrame(tick);
-      }
-    }
-
-    window.requestAnimationFrame(tick);
   }
 }
